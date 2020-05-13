6a6fe7a1930f3a556ee2301826915c7e85d39a69
Camiyah Goins <tokyonote9@gmail.com>
2020-05-13 17:33:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 
 
 def square_array(array)
-  squared = []
+  squared = [] #must set the (squared = []) inside the method otherwise it will never call it if its outside the method block
   array.each { |element| squared << element ** 2 }
-  squared
+  #each element in the array is squared then pushed into the empty array called squared, otherwise each shows the original array instead of the squared numbers
+  squared #squared is then called upon implicitly
 end 
