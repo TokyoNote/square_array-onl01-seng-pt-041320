b6e688c1f5c0dc792cd7721333a1e28dc62766d8
Camiyah Goins <tokyonote9@gmail.com>
2020-05-13 17:24:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,12 @@
 numbers = [1,2,3]
 new_numbers = [4,5,8,12]
-squared = []
 
 def square_array(numbers)
+  squared = []
   numbers.each {|number| squared << number ** 2}
 end 
 
 
 square_array(numbers)
+
+square_array(new_numbers)
